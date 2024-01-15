051309a3510d64d7e821bab0cc8efb73720b0ffd
Yosef Zaiman <yosizam123@gmail.com>
2024-01-15 16:26:43 +0200

    nodeJS sqlite3 + lowest price finder by id

--- Index.js
+++ Index.js
@@ -3,8 +3,13 @@ const puppeteer = require('puppeteer');
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto('https://www.youtube.com/');
-	await page.screenshot({path: 'exapmle.png'});
 
-	await browser.close
-})();
+	let id = 930509;
+	await page.goto('https://buff.163.com/goods/'+ id +'?from=market#tab=selling');
+
+	const t = await page.$eval(".f_Strong:first-child", e => e.innerText);
+	console.log(t);
+	
+
+	await browser.close();
+})()
