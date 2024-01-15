76afd9be6f62b57a330992a2d81047208d102328
Yosef Zaiman <yosizam123@gmail.com>
2024-01-15 16:29:06 +0200

    test

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
-	let id = 930509;
+	let id = 0;
 	await page.goto('https://buff.163.com/goods/'+ id +'?from=market#tab=selling');
 
 	const t = await page.$eval(".f_Strong:first-child", e => e.innerText);
